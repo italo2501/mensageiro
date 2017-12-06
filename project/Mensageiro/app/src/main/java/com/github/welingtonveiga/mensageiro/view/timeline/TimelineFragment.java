d9ea28fd7135b1b9231e3a45ee530a98584751d9
Welington Veiga <welington.veiga@gmail.com>
2017-12-06 10:00:34 -0200

    Atualizando o projeto para a versão 25.

--- project/Mensageiro/app/src/main/java/com/github/welingtonveiga/mensageiro/view/timeline/TimelineFragment.java
+++ project/Mensageiro/app/src/main/java/com/github/welingtonveiga/mensageiro/view/timeline/TimelineFragment.java
@@ -12,6 +12,7 @@ import com.github.welingtonveiga.mensageiro.domain.model.Message;
 import com.github.welingtonveiga.mensageiro.domain.services.MessageService;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class TimelineFragment  extends ListFragment {
 
@@ -21,24 +22,48 @@ public class TimelineFragment  extends ListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         // 1 - Exibe diálogo de carregamento.
-        final ProgressDialog loader =  ProgressDialog.show(getActivity(), "Por favor Aguarde ...", "Carregando mensagens.");
-        new AsyncTask<Void, Void, List<Message>>() {
-            @Override
-            protected List<Message> doInBackground(Void... voids) {
-                // 2 - Realiza, em background (outra Thread), tarefa IO bloqueante.
-                return messageService.getLastStatuses();
-            }
+        new MessagesLoaderAsyncTask(getActivity(), this, messageService).execute();
+    }
 
-            @Override
-            protected void onPostExecute(List<Message> statuses) {
-                // 3 - Código executado na thread principal (UI), após realização da tarefa.
-                Activity activity = TimelineFragment.this.getActivity();
-                ListAdapter adapter = new TimelineAdapter(activity, R.layout.fragment_timeline_item,statuses);
-                TimelineFragment.this.setListAdapter(adapter);
 
-                // 4 - Esconde o diálogo de carregando.
-                loader.dismiss();
-            }
-        }.execute();
+
+    private static class MessagesLoaderAsyncTask extends AsyncTask<Void, Void, List<Message>> {
+
+        private final AtomicReference<ProgressDialog> loader = new AtomicReference<>();
+
+        private final Activity activity;
+
+        private final ListFragment fragment;
+
+        private final MessageService service;
+
+        private MessagesLoaderAsyncTask(Activity activity, ListFragment fragment, MessageService service) {
+            this.activity = activity;
+            this.fragment = fragment;
+            this.service = service;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //loader.set(ProgressDialog.show(activity, "Por favor Aguarde ...", "Carregando mensagens."));
+        }
+
+        @Override
+        protected List<Message> doInBackground(Void... voids) {
+            // 2 - Realiza, em background (outra Thread), tarefa IO bloqueante.
+            return service.getLastStatuses();
+        }
+
+        @Override
+        protected void onPostExecute(List<Message> statuses) {
+            // 3 - Código executado na thread principal (UI), após realização da tarefa.
+            Activity activity = fragment.getActivity();
+            ListAdapter adapter = new TimelineAdapter(activity, R.layout.fragment_timeline_item,statuses);
+            fragment.setListAdapter(adapter);
+
+            // 4 - Esconde o diálogo de carregando.
+            //loader.get().dismiss();
+        }
     }
 }
